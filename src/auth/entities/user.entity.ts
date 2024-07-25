538a05200561d17f6c259aee0fb624cff3943889
Irvin Fernandez <ifernandez.estevezjor@gmail.com>
2024-07-25 17:36:51 -0600

    Sección 25: Auth MEAN Nest Backend - Creacion de usuario y login con pruebas en postman

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class User {
+
+    @Prop({unique: true, required: true})
+    email: string;
+
+    @Prop({required: true})
+    name: string;
+
+    @Prop({minlength: 6, required: true})
+    password?: string;
+
+    @Prop({default: true})
+    isActive: boolean;
+
+    @Prop({type: [String], default: ['user']})
+    roles: string[];
+
+}
+
+
+export const UserSchema = SchemaFactory.createForClass(User);
